3a1a89a1e3cc90a10cc3e266b59155834c7d5fa8
webglzhang <493458883@qq.com>
2020-02-05 10:39:58 +0800

    add hot module replacement

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 //判断是不是开发环境
 const isDev = process.env.NODE_ENV === 'development';
@@ -38,11 +39,19 @@ const config = {
 
 //开发环境中Webpack dev server 本地服务端自动编译和 hot replacement 热替换
 if (isDev) {
+
+    config.entry = {
+        app: [
+            'react-hot-loader/patch',
+            path.join(__dirname, '../client/app.js')
+        ]
+    };
+
     config.devServer = {
         host: '0.0.0.0', //任何IP可以进行访问
         port: '8888',    //端口
         contentBase: path.join(__dirname, '../dist'),
-        hot:true,
+        hot: true,
         overlay: {
             errors: true
         },
@@ -51,8 +60,9 @@ if (isDev) {
         historyApiFallback: {
             index: '/public/index.html'
         },
-    }
+    };
+
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
-console.log(config);
 module.exports = config;
